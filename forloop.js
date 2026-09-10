32a2a03a629f07114722f871b534fa33d7019a18
krish kumar <krishcloud10@gmail.com>
2026-09-10 23:19:21 +0530

    concept of for loop and question. of for loops

---
+++ forloop.js
@@ -0,0 +1,67 @@
+// wap to print 1 to 1
+for(let i=1 ; i<=10 ; i++) {
+console.log(i)}
+
+// wap to print 10 to 1
+ for(let i=10;i>=1;i--){
+     console.log(i);
+    }
+
+//  wap to print even 1 to 50
+ for(let i=1; i<=50 ; i++){
+        if(i%2===0){
+       console.log(i)}}
+
+// wap to print sum of between 1 to 10
+// method 
+for(let i=1; i<=10; i++){
+   num=(i*(i+1)/2)}
+   {console.log(num)}
+
+// wap to print the sum of even number between 1 to 100
+// method-1
+for(let i=1;i<=100;i++){
+  if(i%2===0)
+    sum=sum+i}
+console.log(sum)
+
+// method-2
+let sum=0
+for(let i=1;i<=100;i++){
+    if(i%2===0){
+        sum=sum+1}}
+{
+        num=sum*(sum+1) 
+    }
+
+console.log(num)
+
+// wap to print sum of odd number 1 to 100
+ sum=0
+ for(let i=1;i<=100;i++){
+
+    if(i%2==!0){
+        sum=sum+i
+     }
+ }console.log(sum)
+
+// write a program to print the number which print the square 
+// root from 1 to 5
+for(let i=1;i<6;i++)
+{console.log(i*i)}
+
+// wap to print a factorial of a number
+
+fact=1
+for( a=7;a>=1;a-- ){
+    fact=fact*a
+}
+console.log(fact)
+
+// wap to print a table of 5
+let num=5
+for(i=1,i<=10,i++)
+{
+    console.log("5","X",i,"="5*i) 
+}
+

081e916dccbd242e4bba47e233910570314e6e05
krish kumar <krishcloud10@gmail.com>
2026-09-10 23:18:13 +0530

    basic understanding of (if ,else if, else)

---
+++ conditional statement.js
@@ -0,0 +1,50 @@
+//Q. how to print in js
+let a = 100
+console.log(a)
+
+// Q.condition statement start
+
+// Q.are you able to drive or not
+let age = 18
+if (age>=18){
+     console.log("you are eligilble to drive")
+}
+console.log("no beta")
+
+// Q.passing the exam or not
+let marks = 32
+if (marks>=33){
+    console.log("you are pass")
+}
+
+ console.log("you are failed")
+
+//Q. how to find which number is bigger
+let a = 20
+let b = 30
+if (a>b){
+    console.log("print a is bigger")
+} else{
+    console.log('print b is bigger');
+}
+
+// Q.are you eligible to vote we use if and 
+let age = 18;
+(age>=18) ? console.log("you are eligibal to vote") : console.log("you can't vote")
+
+// Q.grading on the basis of their marks
+let grade = 50
+if(grade>=90){
+   console.log("a ")
+
+}else if(grade>=80){
+   console.log("b")
+}else if(grade>70){
+   console.log("c")
+}else {
+   console.log("e")
+}
+// it give answer like that it first it add like 2+3=5 t
+// hen concatination like 58
+let sum = 2 + 3 + "8"
+console.log(sum)

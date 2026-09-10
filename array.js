a4ce486e434009bc5a3a3564fd725f2887746885
krish kumar <krishcloud10@gmail.com>
2026-09-10 23:20:53 +0530

    Add files via upload
    concept and function of array

---
+++ array.js
@@ -0,0 +1,38 @@
+// array
+let a = ["krish","arjun","ayush","harsh",1,2,3]
+// method-1 to print
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+// method-2 print
+for (let i=0;i<=7;i++){
+        console.log(a[i]}
+// method -3 to print the array if we don not know the length of a list
+for(i=0;i<a.length;i++){
+     console.log(a[i])}
+// length is used to find the total length of the array   
+console.log(a.length)
+
+let a = ["krish","arjun","ayush","harsh",1,2,3]
+a[1]="sahildas"
+console.log(a)
+console.log(a.length-1)
+console.log(a[a.length-1])
+
+// sum of the array
+sum=0
+let arr=[5,10,15,16,17]
+for(i=4;i>=0;i--){
+    sum=sum+arr[i]
+}
+console.log(sum)
+
+// print the array with s.no
+ let arr_1=["mango","orange","cheery","banana"]
+ for(i=0;i<=3;i++)
+    
+    console.log(i,arr_1[i])
